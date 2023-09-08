5d95ff2feaea595acf348787d9f97647dee9e2d6
asbdu263 <144414212+asbdu263@users.noreply.github.com>
2023-09-08 10:53:50 +0530

    Create Factorial.py

---
+++ Factorial.py
@@ -0,0 +1,10 @@
+n=int(input("enter a number"))
+fact=1
+if n1<0:
+print("plese enter positive integer")
+elif n==0:
+print("factorial of 0 is 1")
+else:
+for i in range(n,n1+1)
+fact=fact*i
+print("factorial of",n1,"is:",fact)
